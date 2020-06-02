c023647337b5cb57371dcfea5c380dc5800ce10d
itimika_next <>
2020-06-02 22:25:49 +0900

    [add] Headerコンポーネントの追加
    一旦サンプルをそのまま利用
    https://adminlte.io/docs/3.0/components/main-header.html

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div>
-    <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link>|
-        <router-link to="/about">About</router-link>
-      </div>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "@/components/Header.vue";
 
-#nav {
-  padding: 30px;
-}
+@Component({
+  components: {
+    Header
+  }
+})
+export default class App extends Vue {}
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
