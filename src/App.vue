d667fc3ce17fc2365b4964ee24fbb8ebb49f6315
itimika_next <>
2020-06-02 22:32:30 +0900

    [add] サイドメニューの追加
    一旦サンプルをそのまま実装
    https://adminlte.io/docs/3.0/components/main-sidebar.html

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <Header />
-    <router-view />
+    <SideMenu />
+    <div class="content-wrapper">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from "@/components/Header.vue";
+import SideMenu from "@/components/SideMenu.vue";
 
 @Component({
   components: {
-    Header
+    Header,
+    SideMenu
   }
 })
 export default class App extends Vue {}
